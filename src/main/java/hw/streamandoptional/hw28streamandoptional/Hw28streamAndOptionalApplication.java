0a04cd145e605f9a74c3976371087b77518aa04a
user <kozyan94@gmail.com>
2022-10-15 00:00:10 +0300

    hw 2.8 stream

---
+++ src/main/java/hw/streamandoptional/hw28streamandoptional/Hw28streamAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package hw.streamandoptional.hw28streamandoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw28streamAndOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw28streamAndOptionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hw/streamandoptional/hw28streamandoptional/Hw28streamAndOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package hw.streamandoptional.hw28streamandoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw28streamAndOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
